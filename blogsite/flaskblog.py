b3485cd2cd67ef350b595ae88ea5a852351e3cf4
divyasheel <dk0018w@gmail.com>
2020-05-12 16:18:24 +0530

    added a login and registration form

--- blogsite/flaskblog.py
+++ blogsite/flaskblog.py
@@ -1,10 +1,14 @@
 from flask import Flask;
 from flask import render_template;
 from flask import url_for;
+from flask import flash, redirect;
 
+from forms import RegistrationForm, LoginForm
 
 app = Flask( __name__ );  '''creates an object named app of class Flask'''
-                           
+
+app.config['SECRET_KEY'] = '479ac0a34b4290c30baffefc740a1d5b';
+
 posts = [
         {
             'author': "Divyasheel", 
@@ -29,7 +33,26 @@ def home():
 def about():
     return render_template('about.html', title = "About");
 
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm();
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'));
+    return render_template('register.html', title = 'Register' , form = form);
 
+
+@app.route("/login", methods= ["GET", "POST"])
+def login():
+    form = LoginForm();
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash( "You have been logged in !", "success");
+            return redirect(url_for("home"));
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+ 
+    return render_template('login.html', title = 'Login' , form = form);
 
 if __name__ =="__main__":
     app.run(debug=True)
--- blogsite/forms.py
+++ blogsite/forms.py
@@ -1,7 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField,BooleanField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistrationForm( FlaskForm ):      """inherits from FlaskForm"""
-    username = StringField('Username', validators = [);
+class RegistrationForm( FlaskForm ):      
+    username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 20)]);
+
+    email = StringField('Email', validators = [DataRequired(), Email()]);
+
+    password = PasswordField('Password', validators=[DataRequired()]);
+
+    confirm_password = PasswordField('Confirm Password' , validators = [DataRequired(), EqualTo('password')] );
+
+    submit = SubmitField('Sign up');
+
+
+class LoginForm( FlaskForm ):      
+   
+    email = StringField('Email', validators = [DataRequired(), Email()]);
+
+    password = PasswordField('Password', validators=[DataRequired()]);
+
+    remember = BooleanField('Remember me');
+    submit = SubmitField('Login');
+
