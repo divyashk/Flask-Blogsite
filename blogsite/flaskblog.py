393169dfd9840d073c96e161484ae5cad3acad9d
Divyasheel <dk0018w@gmail.com>
2020-05-23 14:22:58 +0530

    added database connectivity using SQLAlchemy

--- blogsite/flaskblog.py
+++ blogsite/flaskblog.py
@@ -2,27 +2,42 @@ from flask import Flask;
 from flask import render_template;
 from flask import url_for;
 from flask import flash, redirect;
+from flask_sqlalchemy import SQLAlchemy;
 
 from forms import RegistrationForm, LoginForm
 
-app = Flask( __name__ );  '''creates an object named app of class Flask'''
+from datetime import datetime
 
+app = Flask( __name__ );                                                       '''creates an object named app of class Flask'''
 app.config['SECRET_KEY'] = '479ac0a34b4290c30baffefc740a1d5b';
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db';                   '''stores the site.db database at the current working directory'''
+db = SQLAlchemy(app);                                                          '''creates an object named db which is instance of class SQLAlchemy'''
 
-posts = [
-        {
-            'author': "Divyasheel", 
-            'title': "Blog Site",
-            'content': "First Post content",
-            'date_posted':"April 20, 2018"
-        },
-        {
-            'author': "Divyasheel 2", 
-            'title': "Blog Site 2",
-            'content': "Second Post content",
-            'date_posted':"April 21, 2018"
-        }
-]
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique = True, nullable = False);
+    email = db.Column(db.String(120), unique = True, nullable = False);
+    image_file = db.Column(db.String(20), nullable = False, default = "default.jpg");
+    password = db.Column(db.String(60), nullable = False);
+
+    posts = db.relationship('Post', backref='Users', lazy = True);               '''creates a link btw User and Post class'''
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True);
+    title = db.Column(db.String(100), nullable=False);
+    date_posted = db.Column (db.DateTime, nullable = False, default = datetime.utcnow);
+    content = db.Column(db.Text , nullable=False);
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False);          '''stores the id of the User to which the post is later linked'''
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')";
+
+
 
 
 @app.route("/")        
