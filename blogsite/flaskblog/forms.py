b4be7aeebcffabac72c3d83f4b2f1a9a436dee8d
Divyasheel <dk0018w@gmail.com>
2020-06-04 04:33:21 +0530

    *added user profile picture *added user info update feature *added posting,updating and deleting posts

--- blogsite/flaskblog/forms.py
+++ blogsite/flaskblog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,BooleanField, PasswordField, SubmitField
+from flask_login import current_user
+from wtforms import StringField,BooleanField, PasswordField, SubmitField, TextAreaField;
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from flaskblog.models import User;
+from flask_wtf.file import FileField, FileAllowed;
+
 
 class RegistrationForm( FlaskForm ):      
     username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 20)]);
@@ -33,3 +36,27 @@ class LoginForm( FlaskForm ):
     remember = BooleanField('Remember me');
     submit = SubmitField('Login');
 
+class UpdateAccountForm( FlaskForm ):      
+    username = StringField('Username', validators = [DataRequired(), Length(min = 2, max = 20)]);
+
+    email = StringField('Email', validators = [DataRequired(), Email()]);
+    picture =  FileField("Update Profile picture", validators=[FileAllowed(['jpg','png'])]);
+    submit = SubmitField('Update');
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first();
+            if user:
+                raise ValidationError("User name already taken. Please try again!");
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first();
+            if email:
+                raise ValidationError("Email already taken. Please try again!");
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()]);
+    content = TextAreaField("Content", validators=[DataRequired()]);
+    
+    submit = SubmitField("Post")
